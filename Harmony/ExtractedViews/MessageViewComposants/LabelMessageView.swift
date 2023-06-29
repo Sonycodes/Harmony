12354fe8ae399fe13a3b54dfee5502d9668f229c
mbrunel <137277733+rouchk@users.noreply.github.com>
2023-06-29 10:09:53 +0200

    rename MessageView to LabelMessageView

--- Harmony/ExtractedViews/MessageViewComposants/LabelMessageView.swift
+++ Harmony/ExtractedViews/MessageViewComposants/LabelMessageView.swift
@@ -7,14 +7,14 @@
 
 import SwiftUI
 
-struct MessageView: View {
+struct LabelMessageView: View {
     var message : String
     var date : String
     var iconDestinataire : String?
     
     var body: some View {
         VStack {
-            HStack {
+            HStack(alignment: .top) {
                 if (iconDestinataire != nil) {
                     IconUserView(icon: iconDestinataire!)
                 }
@@ -45,8 +45,8 @@ struct MessageView: View {
     }
 }
 
-struct MessageView_Previews: PreviewProvider {
+struct LabelMessageView_Previews: PreviewProvider {
     static var previews: some View {
-        MessageView(message: "Salut, est-ce que tu vas participer à l’évènement de demain?", date: "12h25")
+        LabelMessageView(message: "Salut, est-ce que tu vas participer à l’évènement de demain?", date: "12h25", iconDestinataire: "sonia")
     }
 }
