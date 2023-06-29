393ad48da3a5c6e2d904191b607a76d9c0fc136e
Sonycodes <119419160+Sonycodes@users.noreply.github.com>
2023-06-29 11:05:27 +0200

    Revert "Merge branch 'main' of https://github.com/Sonycodes/Harmony"
    This reverts commit 668a82adde364c11aa4db24fe3fc970b0cd1de8e, reversing
    changes made to 85a8503e920b98d92aa9888ba982254ab328ae96.

--- Harmony/Model/Structures/User.swift
+++ Harmony/Model/Structures/User.swift
@@ -165,10 +165,6 @@ class User : Identifiable, Equatable, ObservableObject {
         return conv
     }
     
-    func delConversationByIndex(index: IndexSet) {
-        self.conversations.remove(atOffsets: index)
-    }
-    
     func delConversationLast() {
         self.conversations.removeLast()
     }
